180405cf636761b7b0d61b84891bc508fcb2cd65
tltrus <77125487+tltrus@users.noreply.github.com>
2024-10-21 21:10:31 +0400

    harmonics simple

---
+++ Harmonics. Simple/Harmonics/Classes/Signal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace WpfApp.Classes
+{
+    class Signal
+    {
+        List<Point> points = new List<Point>();
+        double min, max, step;
+        Func<double, double, double> f;
+        Axis axis;
+        int n;
+        Brush brush;
+        double thickness;
+
+        public Signal(Axis axis, double min, double max, double step, Func<double, double, double> f, int n, Brush brush, double thickness)
+        {
+            this.axis = axis;
+            this.min = min;
+            this.max = max;
+            this.step = step;
+            this.f = f;
+            this.n = n;
+            this.brush = brush;
+            this.thickness = thickness;
+        }
+
+        public void Update()
+        {
+            points.Clear();
+            for (double x = min; x < max; x += step)
+            {
+                double y = f(x, n);
+                points.Add(new Point(axis.Xto(x), axis.Yto(y)));    // original signal
+            }
+        }
+
+        public void Drawing(DrawingContext dc) => DrawLine(dc, brush, points, thickness);
+
+        private void DrawLine(DrawingContext dc, Brush brush, List<Point> points, double thickness = 1)
+        {
+            StreamGeometry streamGeometry = new StreamGeometry();
+            using (StreamGeometryContext geometryContext = streamGeometry.Open())
+            {
+                geometryContext.BeginFigure(points[0], false, false);
+                geometryContext.PolyLineTo(points, true, true);
+            }
+            dc.DrawGeometry(null, new Pen(brush, thickness), streamGeometry);
+        }
+    }
+}
---
+++ Harmonics. Simple/Harmonics/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using WpfApp.Classes;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer;
+        Random rnd = new Random();
+        DrawingVisual visual;
+        DrawingContext dc;
+        double width, height;
+        Axis axis;
+        Point mouse;
+        int factor = 25;
+        Func<double, double, double> F = (x, n) => 1/n * Math.Sin(x * n);
+        Func<double, double, double> F_total = (x, n) =>
+        {
+            double result = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                result += 1.0 / i * Math.Sin(x * i);
+            }
+
+            return result;
+        };
+
+        List<Signal> signals = new List<Signal>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            width = g.Width;
+            height = g.Height;
+
+            mouse = new Point();
+            visual = new DrawingVisual();
+
+            axis = new Axis(width, height);
+            axis.SetFactor(factor);
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+
+            Calculated(2);
+
+            Drawing();
+        }
+        private void Calculated(int num)
+        {
+            signals.Clear();
+            for (int i = 1; i < num; ++i)
+            {
+                signals.Add(new Signal(axis, -20, 20, 0.01, F, i, Brushes.White, 0.7));
+            }
+
+            signals.Add(new Signal(axis, -20, 20, 0.01, F_total, num, Brushes.Red, 2));
+        }
+        private void g_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouse = e.GetPosition(g);
+            Drawing();
+        }
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                factor -= 1;
+            }
+            else
+            {
+                // increasing
+                factor += 1;
+            }
+
+            if (factor <= 0) factor = 1;
+            if (factor >= 58) factor = 58;
+
+            Drawing();
+        }
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+            using (dc = visual.RenderOpen())
+            {
+                // axis drawing
+                axis.Draw(dc, visual);
+                axis.SetFactor(factor); // for mouse wheel
+
+                // signals draw
+                foreach (var signal in signals)
+                {
+                    signal.Update();
+                    signal.Drawing(dc);
+                }
+
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+        private void CheckBox_Click(object sender, RoutedEventArgs e) => Drawing();
+        private void timerTick(object sender, EventArgs e) => Drawing();
+        private void sl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var num = (int)e.NewValue;
+
+            if (axis is null) return;
+            Calculated(num);
+
+            lb.Content = num;
+
+            Drawing();
+        }
+    }
+}
